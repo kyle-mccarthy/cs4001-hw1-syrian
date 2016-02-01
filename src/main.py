8d9ce0e7f20766dd060c85b53ce518d49f818f4f
Kyle McCarthy <km@kylemccarthy.io>
2016-01-31 19:59:36 -0600

    export the file with a differnt name

--- src/main.py
+++ src/main.py
@@ -3,4 +3,4 @@ from reader import Reader
 r = Reader()
 r.file_input('../data/SyriaIDPSites2015LateJunHIUDoS.csv', 'latin-1')
 r.import_csv()
-r.export_html_table('../data/exported_data_table.html')
+r.export_html_table('../data/SyrianIDP.html')
